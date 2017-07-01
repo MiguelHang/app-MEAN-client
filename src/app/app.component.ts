8d07f077eb394e09b88be52c3ce39aa132fe73c3
MiguelHang <miguel.hang@gmail.com>
2017-07-01 14:18:28 +0200

    create user services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { UserService } from './services/user.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  public title = 'Music app';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(
+    private _userService:UserService
+  ){
+    this.user = new User('','','','','','ROLE_USER','');
+  }
+
+  ngOnInit(){
+
+  }
+
+  public onSubmit(){
+    console.log(this.user)
+
+    this._userService.signup(this.user).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        let errorMessage = <any>error;
+
+        if(errorMessage != null){
+          console.log(error);
+        }
+      }
+    );
+  }
 }
