41cf527daf398bf2c2fcff6ab1ba066f4a175fef
MiguelHang <miguel.hang@gmail.com>
2017-07-05 18:24:34 +0200

    Login and register working good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,105 @@ import { UserService } from './services/user.services';
 export class AppComponent implements OnInit{
   public title = 'Music app';
   public user: User;
+  public user_register: User;
   public identity;
   public token;
+  public errorMessage;
+  public alertRegister;
 
   constructor(
     private _userService:UserService
   ){
     this.user = new User('','','','','','ROLE_USER','');
+    this.user_register = new User('','','','','','ROLE_USER','');
   }
 
   ngOnInit(){
-
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   public onSubmit(){
-    console.log(this.user)
-
+    // console.log(this.user)
+    //get user data
     this._userService.signup(this.user).subscribe(
       response => {
-        console.log(response);
+        let identity = response;
+        this.identity = identity;
+
+        if(!this.identity._id){
+          alert("el Usuario no etá correctamente identificado");
+        }else{
+          //Crear  elemento en el locaStorage
+          localStorage.setItem('identity', JSON.stringify(identity));
+
+          //get tokenUser
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+
+              if(this.token.length <= 0){
+                alert("el token no se ha generado");
+              }else{
+                //Crear  elemento en el locaStorage para tener el token
+                localStorage.setItem('token', token);
+                this.user = new User('','','','','','ROLE_USER','');
+              }
+            },
+            error => {
+              let errorMessage = <any>error;
+
+              if(errorMessage != null){
+                let body = JSON.parse(error._body)
+                this.errorMessage = body.message;
+                console.log(error);
+              }
+            }
+          );
+        }
       },
       error => {
         let errorMessage = <any>error;
 
         if(errorMessage != null){
+          let body = JSON.parse(error._body)
+          this.errorMessage = body.message;
+          console.log(error);
+        }
+      }
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
+
+  onSubmitRegister(){
+    console.log(this.user_register);
+
+    this._userService.register(this.user_register).subscribe(
+      response => {
+        let user = response.user;
+        this.user_register = user;
+
+        if(!user._id){
+          this.alertRegister = 'error al registrarse';
+        }else{
+          this.alertRegister = 'El registro se ha relizado correctamente, inicia sesión con '+ this.user_register.email;
+          this.user_register = new User('','','','','','ROLE_USER','');
+        }
+      },
+      error => {
+        let errorMessage = <any>error;
+
+        if(errorMessage != null){
+          let body = JSON.parse(error._body)
+          this.alertRegister = body.message;
           console.log(error);
         }
       }
--- src/app/services/user.services.ts
+++ src/app/services/user.services.ts
@@ -6,6 +6,8 @@ import { GLOBAL } from './global';
 
 @Injectable()
 export class UserService{
+  public identity;
+  public token;
   public url: string;
 
   constructor(private _http: Http ){
@@ -16,7 +18,7 @@ export class UserService{
     if(gethash != null){
       user_to_login.gethash = gethash;
     }
-    
+
     let json = JSON.stringify(user_to_login);
     let params = json;
 
@@ -24,5 +26,36 @@ export class UserService{
 
     return this._http.post(this.url + 'login', params, {headers: headers})
                       .map(res => res.json());
+  }
+
+  register(user_to_register){
+    let params = JSON.stringify(user_to_register);
+
+    let headers = new Headers({'Content-Type':'application/json'});
+
+    return this._http.post(this.url + 'register', params, {headers: headers})
+                      .map(res => res.json());
+  }
+
+  getIdentity(){
+    let identity = JSON.parse(localStorage.getItem('identity'));
+
+    if(identity != undefined){
+      this.identity = identity;
+    }else{
+      this.identity = null;
+    }
+    return this.identity;
+  }
+
+  getToken(){
+    let token = localStorage.getItem('token');
+
+    if(token != undefined){
+      this.token = token;
+    }else{
+      this.token = null;
+    }
+    return this.token;
   }
 }
